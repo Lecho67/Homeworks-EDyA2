fad9f3fa073fd4e7de96398de33d7f412d23d52f
Juan Pablo Castaño <jpc4stano@gmail.com>
2024-02-07 17:04:19 -0500

    funciones hasta concat

---
+++ homework.js
@@ -0,0 +1,36 @@
+a = [1,2,3,4,5,6,7,8,9]
+b = [10,11,12,13,14,15,16,17,18];
+console.log(a.concat(b));
+
+console.log(a.copyWithin());
+console.log(a.entries());
+console.log(a.every());
+console.log(a.fill());
+console.log(a.filter());
+console.log(a.find());
+console.log(a.findIndex());
+console.log(a.flat());
+console.log(a.flatMap());
+console.log(a.forEach());
+console.log(a.includes());
+console.log(a.indexOf());
+console.log(a.join());
+console.log(a.keys());
+console.log(a.lastIndexOf());
+console.log(a.map());
+console.log(a.pop());
+console.log(a.push());
+console.log(a.reduce());
+console.log(a.reduceRight());
+console.log(a.reverse());
+console.log(a.shift());
+console.log(a.slice());
+console.log(a.some());
+console.log(a.sort());
+console.log(a.splice());
+console.log(a.toLocaleString());
+console.log(a.toString());
+console.log(a.unshift());
+console.log(a.values());
+
+
