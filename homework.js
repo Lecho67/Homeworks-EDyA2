97fe1aa275a821e5b6afa177be9f25c7313c3b85
Juan Pablo Castaño <jpc4stano@gmail.com>
2024-02-08 08:58:13 -0500

    "Terminado"

--- homework.js
+++ homework.js
@@ -1,36 +1,69 @@
 a = [1,2,3,4,5,6,7,8,9]
 b = [10,11,12,13,14,15,16,17,18];
+m = [1,2,[3,4,5,[6,7,8,9]]];
 console.log(a.concat(b));
 
-console.log(a.copyWithin());
-console.log(a.entries());
-console.log(a.every());
-console.log(a.fill());
-console.log(a.filter());
-console.log(a.find());
-console.log(a.findIndex());
-console.log(a.flat());
-console.log(a.flatMap());
-console.log(a.forEach());
-console.log(a.includes());
-console.log(a.indexOf());
+console.log(a.copyWithin(0,3,5));
+
+iterator = a.entries()
+
+iterator.forEach(x => console.log(x));
+
+console.log(a.every(x => x < 10));
+
+console.log(a.fill("hola",4));
+
+console.log(a.filter(x => x % 2 == 0));
+
+console.log(a.find(x => x + 2 == 4));
+
+console.log(a.findIndex(x => x % 3 == 0));
+
+console.log(m.flat());
+
+console.log(m.flatMap(x => x == 3 ? "jaja soy un tre": `soy un ${x}`));
+
+console.log(a.includes(2));
+
+console.log(a.indexOf(8));
+
 console.log(a.join());
-console.log(a.keys());
-console.log(a.lastIndexOf());
-console.log(a.map());
-console.log(a.pop());
-console.log(a.push());
-console.log(a.reduce());
-console.log(a.reduceRight());
+
+iterador = a.keys()
+iterador.forEach(x => console.log(x));
+
+console.log(a.lastIndexOf(1));
+
+console.log(a.map(x => x+(x/2)));
+
+console.log(b.pop());
+
+console.log(a.push(10));
+
+console.log(a.reduce((x, y) => x + y), 0);
+
+console.log(a.reduceRight((x, y) => x - y,100));
+
 console.log(a.reverse());
+
 console.log(a.shift());
-console.log(a.slice());
-console.log(a.some());
+
+console.log(a.slice(0,2));
+
+console.log(a.some(x => x == 11));
+
 console.log(a.sort());
-console.log(a.splice());
-console.log(a.toLocaleString());
+
+console.log(a.splice(0,0,1));
+
+console.log(a.toLocaleString("es-CO", { style: "currency", currency: "COP" })); 
+
 console.log(a.toString());
-console.log(a.unshift());
-console.log(a.values());
+
+console.log(a.unshift("inicio del array"));
+
+iterador = a.values();
+
+iterador.forEach(x => console.log(x));
 
 
