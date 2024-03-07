27b934c82aa7005044a33c436a8e47d9570c7cbe
jp_castano <jpc4stano@gmail.com>
2024-03-06 19:14:52 -0500

    "Ejemplo implementado"

--- Stacks.js
+++ Stacks.js
@@ -20,6 +20,38 @@ class stack {
         return this.counter;
     }
     print(){
-        console.log(this.items);
+        this.items.forEach(element => {
+            console.log(element.name, element.ISBN, element.Author, element.Editorial);
+        });
     }
 }
+
+class libro {
+    constructor(name, ISBN, Author, Editorial){
+        this.name = name;
+        this.ISBN = ISBN;
+        this.Author = Author;
+        this.Editorial = Editorial;
+    }
+}
+
+const generateRandomString = (num) => {
+    const characters =
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result1 = ' ';
+    const charactersLength = characters.length;
+    for (let i = 0; i < num; i++) {
+      result1 +=
+        characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result1;
+}
+
+const libros = new stack();
+
+for (let i = 0; i < 10; i++) {
+    const ObjLibro = new libro(generateRandomString(10),Math.floor(Math.random()*100000),generateRandomString(10),generateRandomString(10));
+    libros.push(ObjLibro);
+}
+
+libros.print();
